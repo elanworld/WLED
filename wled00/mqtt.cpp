f6d022c868b29496f4be17d7e120deaf1c88d17c
alan <elanworld@163.com>
2022-09-07 09:18:23 +0800

    recovery Visual Studio code format

--- wled00/mqtt.cpp
+++ wled00/mqtt.cpp
@@ -5,11 +5,13 @@
  */
 
 #ifdef WLED_ENABLE_MQTT
-#define MQTT_KEEP_ALIVE_TIME 60  // contact the MQTT broker every 60 seconds
+#define MQTT_KEEP_ALIVE_TIME 60 // contact the MQTT broker every 60 seconds
 JsonArray fxs;
 
-JsonArray getFxs(DynamicJsonDocument doc) {
-  if (fxs.size() > 0) {
+JsonArray getFxs(DynamicJsonDocument doc)
+{
+  if (fxs.size() > 0)
+  {
     DEBUGFS_PRINTLN("use exists fxs json,continue")
     return fxs;
   }
@@ -19,11 +21,16 @@ JsonArray getFxs(DynamicJsonDocument doc) {
   uint16_t nameStart = 0, nameEnd = 0;
   int i = 0;
   bool isNameStart = true;
-  for (uint16_t j = 0; j < jmnlen; j++) {
-    if (pgm_read_byte(JSON_mode_names + j) == '\"' || j == jmnlen - 1) {
-      if (isNameStart) {
+  for (uint16_t j = 0; j < jmnlen; j++)
+  {
+    if (pgm_read_byte(JSON_mode_names + j) == '\"' || j == jmnlen - 1)
+    {
+      if (isNameStart)
+      {
         nameStart = j + 1;
-      } else {
+      }
+      else
+      {
         nameEnd = j;
         char mdn[56];
         uint16_t namelen = nameEnd - nameStart;
@@ -38,18 +45,23 @@ JsonArray getFxs(DynamicJsonDocument doc) {
   return fxs;
 }
 
-void sendState() {
+void sendState()
+{
 #ifdef WLED_USE_DYNAMIC_JSON
   DynamicJsonDocument doc(JSON_BUFFER_SIZE);
 #else
-  if (!requestJSONBufferLock(15)) return;
+  if (!requestJSONBufferLock(15))
+    return;
 #endif
   char subuf[38];
   strlcpy(subuf, mqttDeviceTopic, 33);
   strcat_P(subuf, PSTR("/state"));
-  if (bri == 0) {
+  if (bri == 0)
+  {
     doc["state"] = "OFF";
-  } else {
+  }
+  else
+  {
     doc["state"] = "ON";
   }
   doc["brightness"] = bri;
@@ -59,55 +71,72 @@ void sendState() {
   color["g"] = col[1];
   color["b"] = col[2];
   JsonArray fsx = getFxs(doc);
-  if (fsx.size() > effectCurrent) {
+  if (fsx.size() > effectCurrent)
+  {
     doc["effect"] = fsx.getElement(effectCurrent);
   }
   String payload;
   serializeJson(doc, payload);
-  mqtt->publish(subuf, 0, false, payload.c_str());  // do not retain message
+  mqtt->publish(subuf, 0, false, payload.c_str()); // do not retain message
   releaseJSONBufferLock();
 }
 
-void setState(String payloadStr, char* payload, char* topic) {
+void setState(String payloadStr, char *payload, char *topic)
+{
   // homeassistant command
-  if (strcmp_P(topic, PSTR("/command")) == 0) {
-    if (payload[0] == '{') {
+  if (strcmp_P(topic, PSTR("/command")) == 0)
+  {
+    if (payload[0] == '{')
+    {
 #ifdef WLED_USE_DYNAMIC_JSON
       DynamicJsonDocument doc(JSON_BUFFER_SIZE);
 #else
-      if (!requestJSONBufferLock(15)) return;
+      if (!requestJSONBufferLock(15))
+        return;
 #endif
       deserializeJson(doc, payloadStr);
       JsonObject commandJson = doc.as<JsonObject>();
       // update bright
-      if (commandJson.containsKey("state")) {
-        if (realtimeOverride == 0) {
+      if (commandJson.containsKey("state"))
+      {
+        if (realtimeOverride == 0)
+        {
           // set realtimeOverride until reboot
           String apireq = "win&LO=2";
           DEBUG_PRINTLN("fake api from json");
           handleSet(nullptr, apireq);
         }
 
-        if (strcmp_P(commandJson["state"], "OFF") == 0) {
+        if (strcmp_P(commandJson["state"], "OFF") == 0)
+        {
           briLast = bri;
           bri = 0;
-        } else if (strcmp_P(commandJson["state"], "ON") == 0) {
-          if (commandJson.containsKey("brightness")) {
-            if (commandJson["brightness"].is<int>()) {
-              if (commandJson["brightness"] > 0) {
+        }
+        else if (strcmp_P(commandJson["state"], "ON") == 0)
+        {
+          if (commandJson.containsKey("brightness"))
+          {
+            if (commandJson["brightness"].is<int>())
+            {
+              if (commandJson["brightness"] > 0)
+              {
                 bri = commandJson["brightness"];
               }
             }
-          } else {
+          }
+          else
+          {
             bri = briLast;
           }
         }
         stateUpdated(CALL_MODE_DIRECT_CHANGE);
         // update color
-        if (commandJson.containsKey("color")) {
+        if (commandJson.containsKey("color"))
+        {
           if (commandJson["color"].containsKey("r") &&
               commandJson["color"].containsKey("g") &&
-              commandJson["color"].containsKey("b")) {
+              commandJson["color"].containsKey("b"))
+          {
             col[0] = commandJson["color"]["r"];
             col[1] = commandJson["color"]["g"];
             col[2] = commandJson["color"]["b"];
@@ -115,12 +144,15 @@ void setState(String payloadStr, char* payload, char* topic) {
           }
         }
         // update fx
-        if (commandJson.containsKey("effect")) {
+        if (commandJson.containsKey("effect"))
+        {
           String apireq = "win&FX=";
           String fx = commandJson["effect"].as<String>();
           JsonArray fxs = getFxs(doc);
-          for (size_t i = 0; i < fxs.size(); i++) {
-            if (fxs.getElement(i).as<String>().compareTo(fx) == 0) {
+          for (size_t i = 0; i < fxs.size(); i++)
+          {
+            if (fxs.getElement(i).as<String>().compareTo(fx) == 0)
+            {
               apireq += i;
               DEBUG_PRINTLN("fake api from json");
               handleSet(nullptr, apireq);
@@ -136,9 +168,11 @@ void setState(String payloadStr, char* payload, char* topic) {
   }
 }
 
-void sendHADiscoveryMQTT() {
+void sendHADiscoveryMQTT()
+{
 #if ARDUINO_ARCH_ESP32 || LWIP_VERSION_MAJOR >= 1
-  if (mqtt == nullptr || !mqtt->connected()) return;
+  if (mqtt == nullptr || !mqtt->connected())
+    return;
   char buf[45], bufc[45], bufa[45];
   strcpy(buf, mqttDeviceTopic);
   strcpy(bufc, mqttDeviceTopic);
@@ -150,7 +184,8 @@ void sendHADiscoveryMQTT() {
 #ifdef WLED_USE_DYNAMIC_JSON
   DynamicJsonDocument doc(JSON_BUFFER_SIZE);
 #else
-  if (!requestJSONBufferLock(15)) return;
+  if (!requestJSONBufferLock(15))
+    return;
 #endif
   doc["schema"] = "json";
   doc["brightness"] = true;
@@ -206,27 +241,36 @@ void sendHADiscoveryMQTT() {
 #endif
 }
 
-void parseMQTTBriPayload(char* payload) {
+void parseMQTTBriPayload(char *payload)
+{
   if (strstr(payload, "ON") || strstr(payload, "on") ||
-      strstr(payload, "true")) {
+      strstr(payload, "true"))
+  {
     bri = briLast;
     stateUpdated(1);
-  } else if (strstr(payload, "T") || strstr(payload, "t")) {
+  }
+  else if (strstr(payload, "T") || strstr(payload, "t"))
+  {
     toggleOnOff();
     stateUpdated(1);
-  } else {
+  }
+  else
+  {
     uint8_t in = strtoul(payload, NULL, 10);
-    if (in == 0 && bri > 0) briLast = bri;
+    if (in == 0 && bri > 0)
+      briLast = bri;
     bri = in;
     stateUpdated(CALL_MODE_DIRECT_CHANGE);
   }
 }
 
-void onMqttConnect(bool sessionPresent) {
+void onMqttConnect(bool sessionPresent)
+{
   //(re)subscribe to required topics
   char subuf[38];
 
-  if (mqttDeviceTopic[0] != 0) {
+  if (mqttDeviceTopic[0] != 0)
+  {
     strlcpy(subuf, mqttDeviceTopic, 33);
     mqtt->subscribe(subuf, 0);
     strcat_P(subuf, PSTR("/col"));
@@ -239,7 +283,8 @@ void onMqttConnect(bool sessionPresent) {
     sendHADiscoveryMQTT();
   }
 
-  if (mqttGroupTopic[0] != 0) {
+  if (mqttGroupTopic[0] != 0)
+  {
     strlcpy(subuf, mqttGroupTopic, 33);
     mqtt->subscribe(subuf, 0);
     strcat_P(subuf, PSTR("/col"));
@@ -255,32 +300,41 @@ void onMqttConnect(bool sessionPresent) {
   DEBUG_PRINTLN(F("MQTT ready"));
 }
 
-void onMqttMessage(char* topic, char* payload,
+void onMqttMessage(char *topic, char *payload,
                    AsyncMqttClientMessageProperties properties, size_t len,
-                   size_t index, size_t total) {
+                   size_t index, size_t total)
+{
   DEBUG_PRINT(F("MQTT msg: "));
   DEBUG_PRINTLN(topic);
 
   // paranoia check to avoid npe if no payload
-  if (payload == nullptr) {
+  if (payload == nullptr)
+  {
     DEBUG_PRINTLN(F("no payload -> leave"));
     return;
   }
   // make a copy of the payload to 0-terminate it
-  char* payloadStr = new char[len + 1];
-  if (payloadStr == nullptr) return;  // no mem
+  char *payloadStr = new char[len + 1];
+  if (payloadStr == nullptr)
+    return; // no mem
   strncpy(payloadStr, payload, len);
   payloadStr[len] = '\0';
   DEBUG_PRINTLN(payloadStr);
 
   size_t topicPrefixLen = strlen(mqttDeviceTopic);
-  if (strncmp(topic, mqttDeviceTopic, topicPrefixLen) == 0) {
+  if (strncmp(topic, mqttDeviceTopic, topicPrefixLen) == 0)
+  {
     topic += topicPrefixLen;
-  } else {
+  }
+  else
+  {
     topicPrefixLen = strlen(mqttGroupTopic);
-    if (strncmp(topic, mqttGroupTopic, topicPrefixLen) == 0) {
+    if (strncmp(topic, mqttGroupTopic, topicPrefixLen) == 0)
+    {
       topic += topicPrefixLen;
-    } else {
+    }
+    else
+    {
       // Non-Wled Topic used here. Probably a usermod subscribed to this topic.
       usermods.onMqttMessage(topic, payloadStr);
       delete[] payloadStr;
@@ -293,28 +347,39 @@ void onMqttMessage(char* topic, char* payload,
 
   // Prefix is stripped from the topic at this point
 
-  if (strcmp_P(topic, PSTR("/col")) == 0) {
-    colorFromDecOrHexString(col, (char*)payloadStr);
+  if (strcmp_P(topic, PSTR("/col")) == 0)
+  {
+    colorFromDecOrHexString(col, (char *)payloadStr);
     colorUpdated(CALL_MODE_DIRECT_CHANGE);
-  } else if (strcmp_P(topic, PSTR("/api")) == 0) {
-    if (payload[0] == '{') {  // JSON API
+  }
+  else if (strcmp_P(topic, PSTR("/api")) == 0)
+  {
+    if (payload[0] == '{')
+    { // JSON API
 #ifdef WLED_USE_DYNAMIC_JSON
       DynamicJsonDocument doc(JSON_BUFFER_SIZE);
 #else
-      if (!requestJSONBufferLock(15)) return;
+      if (!requestJSONBufferLock(15))
+        return;
 #endif
       deserializeJson(doc, payloadStr);
       deserializeState(doc.as<JsonObject>());
       releaseJSONBufferLock();
-    } else {  // HTTP API
+    }
+    else
+    { // HTTP API
       String apireq = "win&";
-      apireq += (char*)payloadStr;
+      apireq += (char *)payloadStr;
       handleSet(nullptr, apireq);
     }
-  } else if (strlen(topic) != 0) {
+  }
+  else if (strlen(topic) != 0)
+  {
     // non standard topic, check with usermods
     usermods.onMqttMessage(topic, payloadStr);
-  } else {
+  }
+  else
+  {
     // topmost topic (just wled/MAC)
     parseMQTTBriPayload(payloadStr);
   }
@@ -322,9 +387,11 @@ void onMqttMessage(char* topic, char* payload,
   delete[] payloadStr;
 }
 
-void publishMqtt() {
+void publishMqtt()
+{
   doPublishMqtt = false;
-  if (!WLED_MQTT_CONNECTED) return;
+  if (!WLED_MQTT_CONNECTED)
+    return;
   DEBUG_PRINTLN(F("Publish MQTT"));
 
   char s[10];
@@ -333,23 +400,23 @@ void publishMqtt() {
   sprintf_P(s, PSTR("%u"), bri);
   strlcpy(subuf, mqttDeviceTopic, 33);
   strcat_P(subuf, PSTR("/g"));
-  mqtt->publish(subuf, 0, true, s);  // retain message
+  mqtt->publish(subuf, 0, true, s); // retain message
 
   sprintf_P(s, PSTR("#%06X"),
             (col[3] << 24) | (col[0] << 16) | (col[1] << 8) | (col[2]));
   strlcpy(subuf, mqttDeviceTopic, 33);
   strcat_P(subuf, PSTR("/c"));
-  mqtt->publish(subuf, 0, true, s);  // retain message
+  mqtt->publish(subuf, 0, true, s); // retain message
 
   strlcpy(subuf, mqttDeviceTopic, 33);
   strcat_P(subuf, PSTR("/status"));
-  mqtt->publish(subuf, 0, true, "online");  // retain message for a LWT
+  mqtt->publish(subuf, 0, true, "online"); // retain message for a LWT
 
-  char apires[1024];  // allocating 1024 bytes from stack can be risky
+  char apires[1024]; // allocating 1024 bytes from stack can be risky
   XML_response(nullptr, apires);
   strlcpy(subuf, mqttDeviceTopic, 33);
   strcat_P(subuf, PSTR("/v"));
-  mqtt->publish(subuf, 0, false, apires);  // do not retain message
+  mqtt->publish(subuf, 0, false, apires); // do not retain message
 
   sendState();
 }
@@ -357,30 +424,37 @@ void publishMqtt() {
 // HA autodiscovery was removed in favor of the native integration in HA
 // v0.102.0
 
-bool initMqtt() {
-  if (!mqttEnabled || mqttServer[0] == 0 || !WLED_CONNECTED) return false;
+bool initMqtt()
+{
+  if (!mqttEnabled || mqttServer[0] == 0 || !WLED_CONNECTED)
+    return false;
 
-  if (mqtt == nullptr) {
+  if (mqtt == nullptr)
+  {
     mqtt = new AsyncMqttClient();
     mqtt->onMessage(onMqttMessage);
     mqtt->onConnect(onMqttConnect);
   }
-  if (mqtt->connected()) return true;
+  if (mqtt->connected())
+    return true;
 
   DEBUG_PRINTLN(F("Reconnecting MQTT"));
   IPAddress mqttIP;
-  if (mqttIP.fromString(mqttServer))  // see if server is IP or domain
+  if (mqttIP.fromString(mqttServer)) // see if server is IP or domain
   {
     mqtt->setServer(mqttIP, mqttPort);
-  } else {
+  }
+  else
+  {
     mqtt->setServer(mqttServer, mqttPort);
   }
   mqtt->setClientId(mqttClientID);
-  if (mqttUser[0] && mqttPass[0]) mqtt->setCredentials(mqttUser, mqttPass);
+  if (mqttUser[0] && mqttPass[0])
+    mqtt->setCredentials(mqttUser, mqttPass);
 
   strlcpy(mqttStatusTopic, mqttDeviceTopic, 33);
   strcat_P(mqttStatusTopic, PSTR("/status"));
-  mqtt->setWill(mqttStatusTopic, 0, true, "offline");  // LWT message
+  mqtt->setWill(mqttStatusTopic, 0, true, "offline"); // LWT message
   mqtt->setKeepAlive(MQTT_KEEP_ALIVE_TIME);
   mqtt->connect();
   return true;
