f804b53170d0e1b85393251011303d9a8577cb8a
alan <elanworld@163.com>
2024-12-26 16:11:48 +0800

    add config for button sensor

--- usermods/button_sensor/button_sensor.h
+++ usermods/button_sensor/button_sensor.h
@@ -10,21 +10,38 @@ void IRAM_ATTR handleSensorInterrupt()
 class ButtonSensorUsermod : public Usermod
 {
 private:
+    bool enable = false;
     int sensorPin = 25; // 设置震动传感器连接的GPIO引脚
-    unsigned long lastTriggerdTime = 0; 
-    unsigned long interval = 300;   
+    bool pullUp = true;  // 设置震动传感器连接的GPIO引脚
+    unsigned long lastTriggerdTime = 0;
+    unsigned long interval = 300;
+
 public:
     void setup() override
     {
-        pinMode(sensorPin, INPUT_PULLDOWN); 
-
-        // 设置GPIO引脚的中断，任何引脚电平变化都会触发中断
-        attachInterrupt(digitalPinToInterrupt(sensorPin), handleSensorInterrupt, CHANGE);
-        DEBUG_PRINTLN("Button Sensor Usermod Initialized");
+        if (!enable)
+        {
+            return;
+        }
+        if (pinManager.allocatePin(sensorPin, false, PinOwner::Button))
+        {
+            // 设置GPIO引脚的中断，任何引脚电平变化都会触发中断
+            attachInterrupt(digitalPinToInterrupt(sensorPin), handleSensorInterrupt, CHANGE);
+            DEBUG_PRINTLN("Button Sensor Usermod Initialized");
+            pinMode(sensorPin, pullUp ? INPUT_PULLDOWN : INPUT_PULLUP);
+        }
+        else
+        {
+            DEBUG_PRINTLN("Button Sensor Usermod allocate Pin fail");
+        }
     }
 
     void loop() override
     {
+        if (!enable)
+        {
+            return;
+        }
         unsigned long currentMillis = millis();
 
         if (currentMillis - lastTriggerdTime >= interval)
@@ -39,13 +56,23 @@ public:
             }
         }
     }
-    // 可选：如果需要将模块配置添加到 WLED 配置中
-    void addToConfig(JsonObject &obj) override
+    // 在config中添加新选项
+    virtual void addToConfig(JsonObject &root)
     {
+        JsonObject top = root.createNestedObject("Shake Sensor");
+        top["enable"] = enable;
+        top["sensor pin"] = sensorPin;
+        top["pull up"] = pullUp;
     }
 
-    bool readFromConfig(JsonObject &obj) override
+    // 从config读取设置
+    virtual bool readFromConfig(JsonObject &root)
     {
-        return true;
+        JsonObject top = root["Shake Sensor"];
+        bool configComplete = !top.isNull();
+        configComplete &= getJsonValue(top["enable"], enable);
+        configComplete &= getJsonValue(top["Sensor Pin"], sensorPin);
+        configComplete &= getJsonValue(top["pull up"], pullUp);
+        return configComplete;
     }
 };
